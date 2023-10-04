abf3313214cc94055e3722cfc3f82843aff881a8
deboraj-roy <deborajroy123@gmail.com>
2023-10-04 21:10:12 +0600

    User Class Created

---
+++ src/Console/CSharpReview/Reflection/Program.cs
@@ -0,0 +1,5 @@
+﻿using Reflection;
+
+Student student = new Student(1, "JON", "NL, 4521", 10029, "017013015018019");
+
+Console.WriteLine(student.GetInfo());
---
+++ src/Console/CSharpReview/Reflection/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int RegNo { get; set; }
+        private string Phone { get; set; }
+
+        public Student(int id, string name, string address, int regno, string phone)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            RegNo = regno;
+            Phone = phone;
+
+        }
+
+        public string GetInfo()
+        {
+            return "ID: " + Id + " Name: " + Name + " Address: " + Address + " Registration Number: " + RegNo;
+
+        }
+    }
+}
