9e6ec50da0634931847ab0f34048af8bee00a005
deboraj-roy <deborajroy123@gmail.com>
2023-10-13 01:23:24 +0600

    FirstDemo.Web app  by DevSkill
    Signed-off-by: deboraj-roy <deborajroy123@gmail.com>

--- src/CsharpReview/Reflection/Program.cs
+++ src/CsharpReview/Reflection/Program.cs
@@ -8,52 +8,22 @@ namespace Reflection.Demo
     {
         static void Main(string[] args)
         {
-            var assembly = Assembly.LoadFrom(@"C:\Users\UseR\source\Git and GitHub\ASP-.NET-B09-Practice\src\CsharpReview\PrintAll\bin\Release\net7.0\PrintAll.dll");
+            var assembly = Assembly.LoadFile(@"C:\Users\UseR\source\Git and GitHub\ASP-.NET-B09-Practice\src\CsharpReview\PrintAll\bin\Debug\net7.0\PrintAll.dll");
 
             foreach (var type in assembly.GetTypes())
             {
                 Console.WriteLine($"Type: {type.Name}");
                 Console.WriteLine("============================");
-                var instance = Activator.CreateInstance(type);
+                //var instance = Activator.CreateInstance(type);
 
                 foreach (var field in type.GetFields(BindingFlags.NonPublic |
                     BindingFlags.Instance |
                     BindingFlags.DeclaredOnly))
                 {
                     Console.WriteLine($"Field: {field.Name}");
-                    field.SetValue(instance, "Frodo");
+                   // field.SetValue(instance, "Frodo");
                 }
-                Console.WriteLine("============================");
-
-                foreach (var method in type.GetMethods(BindingFlags.Public |
-                    BindingFlags.NonPublic |
-                    BindingFlags.Instance |
-                    BindingFlags.DeclaredOnly)
-                    .Where(m => !m.IsSpecialName))
-                {
-                    Console.WriteLine($"Method: {method.Name}");
-                    if (method.GetParameters().Length > 0)
-                    {
-                        method.Invoke(instance, new[] { "Bilbo" });
-                    }
-                    else if (method.ReturnType.Name != "Void")
-                    {
-                        var returnedValue = method.Invoke(instance, null);
-                        Console.WriteLine($"Returned value from method: {returnedValue}");
-                    }
-                    else
-                    {
-                        method.Invoke(instance, null);
-                    }
-                }
-                Console.WriteLine("============================");
-
-                foreach (var property in type.GetProperties())
-                {
-                    Console.WriteLine($"Property: {property.Name}");
-                    var propertyValue = property.GetValue(instance);
-                    Console.WriteLine($"Property value: {propertyValue}");
-                }
+                
 
             }
 
