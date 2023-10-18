05bd4bc2870302fcebdf498641bcf657d8a71316
deboraj-roy <deborajroy123@gmail.com>
2023-10-18 22:48:15 +0600

    ChatGPT version one

---
+++ src/CsharpReview/Assignment01/Program.cs
@@ -0,0 +1,46 @@
+﻿using Assignment01;
+House house = new House
+{
+    Rooms = new List<Room>
+            {
+                new Room
+                {
+                    RoomNumber = "200",
+                    Windows = new List<Window>
+                    {
+                        new Window { Width = 200, Height = 300 },
+                        new Window { Width = 44, Height = 88 }
+                    }
+                },
+                new Room
+                {
+                    RoomNumber = "300",
+                    Windows = new List<Window>
+                    {
+                        new Window { Width = 100, Height = 200 },
+                        new Window { Width = 350, Height = 500 }
+                    }
+                }
+            }
+};
+
+Building building = new Building();
+
+SimpleMapper mapper = new SimpleMapper();
+mapper.Copy(house, building);
+
+PrintBuildingData(building);
+
+
+static void PrintBuildingData(Building building)
+{
+    Console.WriteLine("Building Number: " + building.BuildingNumber);
+    foreach (var room in building.Rooms)
+    {
+        Console.WriteLine("Room Number: " + room.RoomNumber);
+        foreach (var window in room.Windows)
+        {
+            Console.WriteLine("Window Width: " + window.Width + ", Height: " + window.Height);
+        }
+    }
+}
---
+++ src/CsharpReview/Assignment01/SimpleMapper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    public class SimpleMapper
+    {
+        public void Copy(object source, object destination)
+        {
+            if (source == null || destination == null) return;
+
+            var sourceType = source.GetType();
+            var destinationType = destination.GetType();
+
+            // Check if the types are the same
+            if (sourceType != destinationType)
+            {
+                throw new ArgumentException("Source and destination types must be the same.");
+            }
+
+            // Get all properties of the source type
+            var properties = sourceType.GetProperties();
+
+            foreach (var property in properties)
+            {
+                // Check if the property is a simple type or a complex type
+                if (IsSimple(property.PropertyType))
+                {
+                    var value = property.GetValue(source);
+                    var destinationProperty = destinationType.GetProperty(property.Name);
+                    if (destinationProperty != null)
+                    {
+                        destinationProperty.SetValue(destination, value);
+                    }
+                }
+                else if (typeof(IEnumerable).IsAssignableFrom(property.PropertyType))
+                {
+                    var sourceList = (IEnumerable)property.GetValue(source);
+                    var destinationList = (IList)Activator.CreateInstance(property.PropertyType);
+
+                    if (sourceList != null)
+                    {
+                        foreach (var item in sourceList)
+                        {
+                            var itemType = item.GetType();
+                            var destinationItem = Activator.CreateInstance(itemType);
+                            Copy(item, destinationItem);
+                            destinationList.Add(destinationItem);
+                        }
+
+                        var destinationProperty = destinationType.GetProperty(property.Name);
+                        if (destinationProperty != null)
+                        {
+                            destinationProperty.SetValue(destination, destinationList);
+                        }
+                    }
+                }
+                else
+                {
+                    var sourceValue = property.GetValue(source);
+                    var destinationValue = property.GetValue(destination) ?? Activator.CreateInstance(property.PropertyType);
+                    Copy(sourceValue, destinationValue);
+                    property.SetValue(destination, destinationValue);
+                }
+            }
+        }
+
+        private bool IsSimple(Type type)
+        {
+            return type.IsPrimitive || type.IsValueType || type == typeof(string);
+        }
+    }
+}
