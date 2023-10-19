573c934f4d6fe5295a806ba82086cd3697bcd3b9
deboraj-roy <deborajroy123@gmail.com>
2023-10-19 10:34:17 +0600

    Class 05 done

--- src/WebApplication/FirstDemo.Web/Controllers/HomeController.cs
+++ src/WebApplication/FirstDemo.Web/Controllers/HomeController.cs
@@ -15,12 +15,33 @@ namespace FirstDemo.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var user = new UserClass() {
+                Id = 1,
+                Name = "John Doe",
+                Email = "johndoe@example.com",
+                Password = "password123",
+                Phone = "1234567890"
+            };
+            var k = user.Sum(71 , 2);
+            return View(user);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+        public ActionResult UserData() 
+        {
+            var user = new UserClass()
+            {
+                Id = 1,
+                Name = "John Doe",
+                Email = "johndoe@example.com",
+                Password = "password123",
+                Phone = "1234567890"
+            };
+            var k = user.Sum(71, 2);
+            return View(user);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ src/WebApplication/FirstDemo.Web/Models/UserClass.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstDemo.Web.Models
+{
+    public class UserClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; } 
+
+
+
+        public decimal Sum(decimal a, decimal b)
+        {
+            return a * b;
+        }
+    }
+}
