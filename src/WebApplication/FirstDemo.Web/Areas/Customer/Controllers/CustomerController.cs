28240ae0176bada8d9430e5a0dba28f67e071509
deboraj-roy <deborajroy123@gmail.com>
2023-10-29 21:01:54 +0600

    mono-bootstrap-main index page

---
+++ src/WebApplication/FirstDemo.Web/Areas/Customer/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDemo.Web.Areas.Coustomer.Controllers
+{
+    [Area("Customer")]
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
