0779988386fd6ad20acb737887ff0f25cba8e519
deboraj-roy <deborajroy123@gmail.com>
2023-11-17 22:52:37 +0600

    Singleton design pattern in C#

---
+++ src/CsharpReview/Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.WriteLine("Singleton, Logger!");
+
+Logger logger1 = Logger.CreateLogger();
+Logger logger2 = Logger.CreateLogger();
+
+if (logger1 != logger2)
+    Console.WriteLine("Same Logger");
