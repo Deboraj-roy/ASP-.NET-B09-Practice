513670a3552e3ea12a4416ed4be4826697832904
deboraj-roy <deborajroy123@gmail.com>
2023-11-17 23:37:48 +0600

    Here, MathOperations is a static class with two static methods (Add and Multiply). You can use these methods without creating an instance of the class.

---
+++ src/CsharpReview/StaticClass/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    public static class MathOperations
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ src/CsharpReview/StaticClass/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+using StaticClass;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Static class: add:" + MathOperations.Add(2, 3));
