169e72ab74a671023508fd32aeea30075dec827d
deboraj-roy <deborajroy123@gmail.com>
2023-11-21 01:57:33 +0600

    Events In C#, when you subscribe a method to an event that expects a parameter, but the method itself doesn't take any parameters, you can still subscribe it, but you need to ensure that the method matches the required delegate signature. In this case, the OnTextChanged event expects a delegate of type Action<string>, meaning a method that takes a string parameter.

---
+++ src/CsharpReview/Events/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Events;
+
+Console.WriteLine("Event, World!");
+
+TextBox textBox1 = new TextBox();
+textBox1.OnTextChanged += PrintText;
+textBox1.OnTextChanged += PrintText2;
+
+textBox1.AddText("hello");
+
+textBox1.OnTextChanged -= PrintText2;
+textBox1.AddText(textBox1.Text + " hello");
+
+
+void PrintText2(string obj)
+{
+    Console.WriteLine(obj);
+}
+
+void PrintText(string obj)
+{
+    Console.WriteLine(obj);
+
+}
+
+textBox1.OnTextChanged += PrintText2;
+
+
