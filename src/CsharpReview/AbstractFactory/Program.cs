9b69cb191253b1279cc95d84d38f33e8f3f4c3e0
deboraj-roy <deborajroy123@gmail.com>
2023-11-21 21:50:43 +0600

    Abstract Factory Pattern

---
+++ src/CsharpReview/AbstractFactory/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractFactory;
+using AbstractFactory.BMW;
+
+Console.WriteLine("Abstract Factory Pattern, World!");
+CarFactory factory = new BMWFactory();
+IEngine engine = factory.CreateEngine();
+ITire tire = factory.CreateTire();
+IHeadLight headLight = factory.CreateHeadLight();
