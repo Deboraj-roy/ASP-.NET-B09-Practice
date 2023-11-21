efb3ddaa7747faf91622d8416757c4b4bf74179e
deboraj-roy <deborajroy123@gmail.com>
2023-11-21 21:03:01 +0600

    Factory Pattern

---
+++ src/CsharpReview/FactoryPattern/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FactoryPattern;
+
+Console.WriteLine("Factory Pattern, World!");
+
+CarFactory factory = new BMWFactory();
+ICar car = factory.Create("Red", "XO20N9", 2100);
