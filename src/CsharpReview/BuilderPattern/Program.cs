662dd86b92e9171e772d1a19129d50b432a86cb2
deboraj-roy <deborajroy123@gmail.com>
2023-11-21 21:29:19 +0600

    BuilderPattern Connection String Builder

---
+++ src/CsharpReview/BuilderPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BuilderPattern;
+
+Console.WriteLine("BuilderPattern, World!");
+
+string connectionString = 
+    new ConnectionStringBuilder("localhost", "AspnetB9")
+    .UseMultipleActiveRecords()
+    .SetCredentials("aspnetb9", "123456")
+    .UsePort(2222)
+    .GetConnectionString();
+
+Console.WriteLine(connectionString);
