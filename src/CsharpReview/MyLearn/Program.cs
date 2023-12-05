212683001f4c7635d17008af96a3af7ccc4eeec1
deboraj-roy <deborajroy123@gmail.com>
2023-12-05 20:28:32 +0600

    Create Architecture DDD

---
+++ src/CsharpReview/MyLearn/Program.cs
@@ -0,0 +1,11 @@
+﻿dynamic dynamicVariable = 10;
+Console.WriteLine(dynamicVariable); // Outputs 10
+
+dynamicVariable = "Hello, dynamic!";
+Console.WriteLine(dynamicVariable); // Outputs Hello, dynamic!
+
+//[]dynamicVariable = []{ 2,3,4,5,56,76};
+Console.WriteLine(dynamicVariable);
+ 
+int[] ints = new int[] { 2, 3, 4, 5, 56, 76 };
+ 
