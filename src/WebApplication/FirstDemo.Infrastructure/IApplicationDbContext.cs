41da5f15bc5ff1d9a24032ad1891e19d282b8c76
deboraj-roy <deborajroy123@gmail.com>
2023-12-26 16:49:19 +0600

    Class 20 3rd Commit

--- src/WebApplication/FirstDemo.Infrastructure/IApplicationDbContext.cs
+++ src/WebApplication/FirstDemo.Infrastructure/IApplicationDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FirstDemo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstDemo.Infrastructure
 {
     public interface IApplicationDbContext
     {
+        DbSet<Course> Courses { get; set; }
     }
 }
