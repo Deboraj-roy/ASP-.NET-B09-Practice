e8564a5b3af926085b716596c5bbe675743c3024
deboraj-roy <deborajroy123@gmail.com>
2024-01-03 17:18:34 +0600

    ApplicationDbContext

--- src/Exam1/Exam1.Infrastructure/ApplicationDbContext.cs
+++ src/Exam1/Exam1.Infrastructure/ApplicationDbContext.cs
@@ -11,6 +11,31 @@ namespace Exam1.Infrastructure
 {
     public class ApplicationDbContext : IdentityDbContext, IApplicationDbContext
     {
-        public DbSet<Product> Products { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private readonly string _connectionString;
+        private readonly string _migrationAssembly;
+        public ApplicationDbContext(string connectionString, string migrationAssembly)
+        {
+            _connectionString = connectionString;
+            _migrationAssembly = migrationAssembly;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString,
+                    x => x.MigrationsAssembly(_migrationAssembly));
+
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
--- src/Exam1/Exam1.Web/Areas/Admin/Controllers/ProductController.cs
+++ src/Exam1/Exam1.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Autofac;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Exam1.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly ILifetimeScope _scope;
+        private readonly ILogger<ProductController> _logger;
+
+
+        public ProductController(ILifetimeScope scope, ILogger<ProductController> logger)
+        { 
+            _logger = logger;
+            _scope = scope;
+        }
         public IActionResult Index()
         {
             return View();
