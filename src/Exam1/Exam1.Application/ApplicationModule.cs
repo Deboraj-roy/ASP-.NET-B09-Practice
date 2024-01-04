ce3c0175f01783bbc0d4914a4b94f8ba62963584
deboraj-roy <deborajroy123@gmail.com>
2024-01-04 18:11:57 +0600

    3 Module added

--- src/Exam1/Exam1.Application/ApplicationModule.cs
+++ src/Exam1/Exam1.Application/ApplicationModule.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Autofac;
+using Exam1.Application.Features;
+using Exam1.Domain.Features;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exam1.Application
+namespace Exam1.Application 
 {
-    public class ApplicationModule
+    public class ApplicationModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+           builder.RegisterType<ProductManagementServices>().As<IProductManagementServices>()
+                .InstancePerLifetimeScope();
+        }
+
     }
 }
--- src/Exam1/Exam1.Infrastructure/InfrastructureModule.cs
+++ src/Exam1/Exam1.Infrastructure/InfrastructureModule.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Autofac;
+using Exam1.Application;
+using Exam1.Domain.Repositories;
+using Exam1.Infrastructure.Repository;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exam1.Infrastructure
 {
-    public class InfrastructureModule
+    public class InfrastructureModule : Module
     {
+        private readonly string _conncetionString;
+        private readonly string _migrationAssembly;
+
+        public InfrastructureModule(string conncetionString, string migrationAssembly)
+        {
+            _conncetionString = conncetionString;
+            _migrationAssembly = migrationAssembly;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApplicationDbContext>().AsSelf()
+                .WithParameter("conncetionString", _conncetionString)
+                .WithParameter("migrationAssembly", _migrationAssembly)
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<ApplicationDbContext>().As<IApplicationDbContext>()
+                .WithParameter("conncetionString", _conncetionString)
+                .WithParameter("migrationAssembly", _migrationAssembly)
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<IApplicationUnitofWork>().As<IApplicationUnitofWork>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<ProductRepository>().As<IProductRepository>()
+                .InstancePerLifetimeScope();
+        }
     }
 }
