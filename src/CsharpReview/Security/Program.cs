86da6484855deb3b4162969b11fe5b90f8e82099
deboraj-roy <deborajroy123@gmail.com>
2024-01-12 00:19:41 +0600

    SQL Injection using String Catenation

---
+++ src/CsharpReview/Security/Program.cs
@@ -0,0 +1,33 @@
+﻿using Security;
+
+var connectionString = "Server=.\\SQLEXPRESS;Database=AspnetB9;User Id=aspnetb9;Password=123456;Trust Server Certificate=True;";
+AdonetUtility adonetUtility = new AdonetUtility(connectionString);
+// ' or 1=1;drop table Courses;--
+Console.Write("Your Query:");
+var title = Console.ReadLine();
+
+var sql = "select * from Courses where title = '"+ title +"'";
+var data = adonetUtility.GetData(sql);
+
+/*
+// ' or 1=1;select * from Courses;--
+ * 
+var sql = "select * from Courses where title = @title";
+var data = adonetUtility.GetData(sql, new Dictionary<string, object>
+{
+    { "title", title }
+});
+
+*/
+if (data is not null)
+{
+    foreach (var row in data)
+    {
+        foreach (var col in row)
+        {
+            Console.Write(col);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
