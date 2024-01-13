893ad317587bfac9632db16f4dd674a4f966d734
deboraj-roy <deborajroy123@gmail.com>
2024-01-13 13:35:35 +0600

    ApplicationUnitOfWork

--- src/Exam1/Exam1.Infrastructure/ApplicationUnitOfWork.cs
+++ src/Exam1/Exam1.Infrastructure/ApplicationUnitOfWork.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Exam1.Application;
+using Exam1.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,12 @@ using System.Threading.Tasks;
 
 namespace Exam1.Infrastructure
 {
-    public class ApplicationUnitOfWork
+    public class ApplicationUnitOfWork : UnitOfWork, IApplicationUnitOfWork
     {
+        public IProductRepository ProductRepository { get; private set; }
+        public ApplicationUnitOfWork(IProductRepository productRepository, IApplicationDbContext dbContext) : base((DbContext)dbContext)
+        {
+            ProductRepository = productRepository;
+        }
     }
 }
