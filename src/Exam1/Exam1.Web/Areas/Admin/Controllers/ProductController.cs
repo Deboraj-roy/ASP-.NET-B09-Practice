5dff2778c79984ccceb1536e3099b59bbaf97e2f
deboraj-roy <deborajroy123@gmail.com>
2024-01-13 14:46:02 +0600

    Data Inserted

--- src/Exam1/Exam1.Web/Areas/Admin/Controllers/ProductController.cs
+++ src/Exam1/Exam1.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,13 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Autofac;
+using Exam1.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Exam1.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly ILifetimeScope _scope;
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(ILifetimeScope scope, ILogger<ProductController> logger)
+        {
+            _scope = scope;
+            _logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            var model = _scope.Resolve<ProductCreateModel>();
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductCreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Resolve(_scope);
+                    await model.CtrateProductAsync();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message, "Server Error");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Update()
+        {
+            var model = _scope.Resolve<ProductCreateModel>();
+
+            return View(model);
         }
     }
 }
