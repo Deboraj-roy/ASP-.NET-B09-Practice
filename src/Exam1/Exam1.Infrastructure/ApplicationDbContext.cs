798a0c6d816631faa429cd0f9db8c851d82a9004
deboraj-roy <deborajroy123@gmail.com>
2024-01-13 13:29:30 +0600

    ApplicationDbContext

--- src/Exam1/Exam1.Infrastructure/ApplicationDbContext.cs
+++ src/Exam1/Exam1.Infrastructure/ApplicationDbContext.cs
@@ -1,7 +1,28 @@
-﻿namespace Exam1.Infrastructure
-{
-    public class ApplicationDbContext
-    {
+﻿using Exam1.Domain.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
+namespace Exam1.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext, IApplicationDbContext
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssembly;
+        public ApplicationDbContext(string connectionString, string migrationAssembly)
+        {
+            _connectionString = connectionString;
+            _migrationAssembly = migrationAssembly;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString,
+                    x=> x.MigrationsAssembly(_migrationAssembly));
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
     }
 }
