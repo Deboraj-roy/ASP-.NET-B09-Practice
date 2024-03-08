128a252ef1f7a8589c616957398e396cacc24282
deboraj-roy <deborajroy123@gmail.com>
2024-03-08 19:56:04 +0600

    server IP get in server not on Local server

---
+++ src/DemoServerAWS/DemoServerAWS/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using DemoServerAWS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoServerAWS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(ViewBag);
+        }
+
+        public IActionResult Privacy()
+        {
+            // Get the server's IP address
+            ViewBag.IpAddress = HttpContext.Connection.RemoteIpAddress?.ToString();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/DemoServerAWS/DemoServerAWS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoServerAWS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
